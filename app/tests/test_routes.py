603fc9d61268ffe7957080832fffa225c5c48352
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-10-30 12:11:42 -0400

    added initial tests for predict endpoint, testing desired behavior

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -2,14 +2,78 @@ from flask import Flask
 
 from app.handlers.routes import configure_routes
 
+# import logging
+
+
+
+app = Flask(__name__)
+configure_routes(app)
+
+
 
 def test_base_route():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/'
+    with app.test_client() as client:
 
-    response = client.get(url)
+        url = '/'
 
-    assert response.status_code == 200
-    assert response.get_data() == b'try the predict route it is great!'
+        response = client.get(url)
+
+        assert response.status_code == 200
+        assert response.get_data() == b'try the predict route it is great!'
+
+def test_predict_route():
+    with app.test_client() as client:
+
+        url = '/predict'
+
+        # testing valid args
+
+        response = client.get(url, query_string={
+            "age": 19,
+            "health": 1,
+            "absences": 17,
+        })
+
+        assert response.status_code == 200
+
+        # Testing missing args
+        
+        response = client.get(url, query_string={
+            "age": 19,
+            "health": 1,
+        })
+
+        assert response.status_code == 400
+
+        # Testing non-existed args
+        response = client.get(url, query_string={
+            "age": 19,
+            "health": 1,
+            "absences": 17,
+            "grade": 17,
+        })
+
+        assert response.status_code == 200
+
+        # Testing invalid arg type
+        response = client.get(url, query_string={
+            "age": 'NaN',
+            "health": 1,
+            "absences": 17,
+        })
+
+        assert response.status_code == 400
+
+        # Testing malformed request body
+        response = client.get(url, query_string=[19, 1, 17])
+
+        assert response.status_code == 400
+
+def test_invalid_address():
+    with app.test_client() as client:
+        # Testing invalid address
+        response = client.get('/change')
+
+        assert response.status_code == 404
+
+

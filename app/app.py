450a101eb7965462ac450484c8adbe0e7aba9930
lzx0406 <zexuanli@andrew.cmu.edu>
2022-10-30 16:19:48 -0400

    set up swagger docs

--- app/app.py
+++ app/app.py
@@ -1,7 +1,20 @@
 from flask import Flask
 from app.handlers.routes import configure_routes
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.yaml'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "ML-Students-Data"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 configure_routes(app)
 
